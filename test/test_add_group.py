80d08e23da296d0f9bf262199f6a69fe7c2de4f0
Foxfanmedium <32788485+Foxfanmedium@users.noreply.github.com>
2017-11-12 13:17:29 +0300

    Improvements

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from fixture.application import Application
 from model.group import Group
 
 
@@ -16,6 +13,14 @@ def test_add_group(application):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="qwerty", header="qwerty", footer="Test"))
     app.session.logout()
+
+
+def delete_first_group(self):
+    wd = self.app.wd
+    self.open_group_page()
+    wd.find_element_by_name("selected[]").click()
+    wd.find_element_by_name("delete").click()
+    self.return_to_groups_page()
 
 
 def test_add_empty_group(application):
