f6da7a8e3799079a56eae9210e3b118c0d96df1e
rzakirov <rjzakirov@fix.ru>
2018-02-06 09:46:11 +0300

    Помещение всех курсов по Python в общий проект

--- Book_cours/odd.py
+++ Book_cours/odd.py
@@ -1,3 +1,5 @@
+# Berri_P_Izuchaem_Programmirovanie_Na_Python_2017-67
+
 #from datetime import datetime
 #odds = [1, 3, 5, 7, 9, 11, 13, 15, 17 ,19 ,21, 23, 25, 27, 29,
 #        31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55,
