4560c9b84385d6c6d470b558a953cfb4d6bab5e2
rzakirov <rjzakirov@fix.ru>
2018-02-14 10:16:01 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Python_training_barantcev
    #	Stepik/__init__.py

--- Geekbrains_cours/les_01.py
+++ Geekbrains_cours/les_01.py
@@ -1,3 +1,37 @@
-print("Hello, programmer!")
-print("Hello!")
-input()
+#print("Hello, programmer!")
+#print("Hello!")
+#input()
+#=======================================================================================================================
+ #coding: utf-8
+import os
+import psutil #pip install psutil
+
+answer = input("Давайте поработаем? (Y/N)")
+if answer == 'Y':
+    print("Отлично хозяин!")
+    print("Я умею:")
+    print("[1] - выведу список файлов")
+    print("[2] - выведу информацию о системе")
+    print("[3] - выведу список процессов")
+    do = int(input("Укажите номер действия:"))
+
+    if do == 1:
+         print(os.listdir())
+    elif do == 2:
+        pass
+    elif do == 3:
+        print(psutil.pids())
+    else:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
