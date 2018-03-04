049b5a15a08ae2d1d7b4661b7ca24a18aa7fd16e
Foxfanmedium <32788485+Foxfanmedium@users.noreply.github.com>
2018-03-04 09:42:38 +0300

    New commit

--- Book_cours/chapter_2/panic.py
+++ Book_cours/chapter_2/panic.py
@@ -10,16 +10,41 @@
 # new_phrase = ''.join(plist)
 # print(new_phrase)
 #==============================================================================================
+# phrase = "Don`t panic!"
+# plist = list(phrase)
+# for i in range(4):
+#     plist.pop()
+# plist.pop(0)
+# plist.remove("`")
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
+# new_phrase = ''.join(plist)
+# print(new_phrase)
+#==============================================================================================
+# string = "The Hitchhiker's Guide to the Galaxy"
+# backwards = string[::-1]
+# print(backwards)
+# string_1 = "The Hitchhiker's Guide to the Galaxy"
+# backwards_1 = string_1[::2]
+# print(backwards_1)
+#==============================================================================================
+
 phrase = "Don`t panic!"
 plist = list(phrase)
-for i in range(4):
-    plist.pop()
-plist.pop(0)
-plist.remove("`")
-plist.extend([plist.pop(), plist.pop()])
-plist.insert(2, plist.pop(3))
-new_phrase = ''.join(plist)
+print(phrase)
+print(plist)
+
+new_phrase = ''.join(plist[1:3])
+new_phrase = new_phrase + ''.join([plist[5], plist[4], plist[7], plist[6]])
 print(new_phrase)
+
+
+
+
+
+
+
+
 
 
 
---
+++ GeekBrains_PreCourse/Lesson_1/Lesson_2.py
@@ -0,0 +1,55 @@
+# a =5
+# b = 6
+# c = 8
+# a, b, c = c, b , a
+# print(a, b, c)
+#======================================================================================
+# email = 'test@mail.ru'
+# print(email[4])
+
+
+name = 'pEtrOv ilIa  il'
+print(name.title())
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.find('il'))
+print(name.find('il', 6)) #  случае если несколько таких сочетаний, но надо найти с какого-то символа-индекса
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
